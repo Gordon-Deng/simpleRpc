e91b4e8ac894021cdd2e6b9313debfb72cdb81e6
Gordon <gordondeng@foxmail.com>
2021-08-20 00:01:45 +0800

    finish server

--- simple-server/src/main/java/com/gordon/rpc/simple/server/remote/OrderServiceImpl.java
+++ simple-server/src/main/java/com/gordon/rpc/simple/server/remote/OrderServiceImpl.java
@@ -1,11 +1,13 @@
 package com.gordon.rpc.simple.server.remote;
 
 import com.gordon.rpc.annotation.SRpcProvider;
+import com.gordon.rpc.simple.api.dto.OrderDTO;
+import com.gordon.rpc.simple.api.service.OrderService;
 
 import java.util.Date;
 
-@SRpcProvider(name="com.orc.rpc.example.api.service.OrderService", version = "1.0.0")
-public class OrderServiceImpl {
+@SRpcProvider(name="com.gordon.rpc.simple.api.service.OrderService", version = "1.0.0")
+public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderDTO getOrderById(String orderNo) {
--- simple-server/src/main/java/com/gordon/rpc/simple/server/remote/ProductServiceImpl.java
+++ simple-server/src/main/java/com/gordon/rpc/simple/server/remote/ProductServiceImpl.java
@@ -1,4 +1,18 @@
 package com.gordon.rpc.simple.server.remote;
 
-public class ProductServiceImpl {
+import com.gordon.rpc.annotation.SRpcProvider;
+import com.gordon.rpc.simple.api.dto.ProductDTO;
+import com.gordon.rpc.simple.api.service.ProductService;
+
+@SRpcProvider(name="com.gordon.rpc.simple.api.service.ProductService", version = "1.0.0")
+public class ProductServiceImpl implements ProductService {
+
+    @Override
+    public ProductDTO getProductById(Long id) {
+        ProductDTO dto = new ProductDTO();
+        dto.setCategory("A类");
+        dto.setProductName("产品名称");
+        return dto;
+    }
+
 }
