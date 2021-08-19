3f2a7a38e43ee00f88430cd5eb968c4426657c9a
Gordon <gordondeng@foxmail.com>
2021-08-19 23:57:01 +0800

    add OrderServiceImpl

--- simple-server/src/main/java/com/gordon/rpc/simple/server/remote/OrderServiceImpl.java
+++ simple-server/src/main/java/com/gordon/rpc/simple/server/remote/OrderServiceImpl.java
@@ -1,4 +1,17 @@
 package com.gordon.rpc.simple.server.remote;
 
+import com.gordon.rpc.annotation.SRpcProvider;
+
+import java.util.Date;
+
+@SRpcProvider(name="com.orc.rpc.example.api.service.OrderService", version = "1.0.0")
 public class OrderServiceImpl {
+
+    @Override
+    public OrderDTO getOrderById(String orderNo) {
+        OrderDTO dto = new OrderDTO();
+        dto.setOrderNo(orderNo);
+        dto.setTime(new Date().toString());
+        return dto;
+    }
 }
